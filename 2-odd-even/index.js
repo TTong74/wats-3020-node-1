faf64b718ab241d4b3598323173a9e36e45a88fd
TTong74 <ttong@seattleu.edu>
2020-01-14 23:20:49 -0800

    finished odd-even

--- 2-odd-even/index.js
+++ 2-odd-even/index.js
@@ -9,15 +9,17 @@ let input = getargs.getIntegerArg();
 /**** Do not modify anything above this line ****/
 
 // test that input is a number
-// TODO use the if statement to check input using isNaN and NOT Number.isInteger 
-if () {
-  // TODO use console.log to provide a usage statement when user forgets to enter a number
+// use the if statement to check input using isNaN and NOT Number.isInteger 
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log('usage: node 2-odd-even <integer>');
 } else {
-  // TODO use the if statement to test if integer is divisible by 2
-  if () {
-    //TODO use console.log with a template literal to report that the number is even
-
+  // use the if statement to test if integer is divisible by 2
+  if (input % 2 == 0) {
+    // use console.log with a template literal to report that the number is even
+    console.log(input + ' is even!');
+    //console.log('${input} is even!') template literal isn't working
   } else {
-    //TODO use console.log with a template literal to report that the number is odd
+    // use console.log with a template literal to report that the number is odd
+    console.log(input + ' is odd!');
   }
 }
