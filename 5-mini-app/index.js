57f5205a0e39dd0024c434f8f2dfdf292dc548f5
TTong74 <ttong@seattleu.edu>
2020-01-15 00:59:28 -0800

    added Math.floor

--- 5-mini-app/index.js
+++ 5-mini-app/index.js
@@ -14,12 +14,12 @@ let input = getargs.getIntegerArg();
 if (isNaN(input) || !Number.isInteger(input)) {
     console.log('Usage: 5-mini-app <integer>');
   } else {
-    let dollar = (input / 100).toFixed();
+    let dollar = Math.floor(input / 100);
     let penny = input % 100;
     let dollars = (dollar == 1) ? "dollar" : "dollars";
     let cents = (penny == 1) ? "cent" : "cents";
     let pennies = (input == 1) ? "penny" : "pennies";
-    console.log(`${input} ${pennies} is ${dollar} ${dollars} and ${penny} ${cents}`)
+    console.log(`${input} ${pennies} is ${dollar} ${dollars} and ${penny} ${cents}.`)
   }
   
 // * convert Celsius temperature to Fahrenheit F =(C * 9/5) + 32.00
@@ -30,5 +30,5 @@ if (isNaN(input) || !Number.isInteger(input)) {
     let fahrenheit = (input * (9/5) + 32.00).toFixed();
     let degreesF = (fahrenheit == 1) ? "degree" : "degrees";
     let degreesC = (input == 1) ? "degree" : "degrees";
-    console.log(`${input} ${degreesC} Celcius is ${fahrenheit} ${degreesF} Fahrenheit`);
+    console.log(`${input} ${degreesC} Celcius is ${fahrenheit} ${degreesF} Fahrenheit.`);
   }
